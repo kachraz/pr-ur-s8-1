27f2aaf6ed57652b58fd66930c74dc7120f55955
kachraz <kachraz@tuta.io>
2024-09-07 17:53:49 +0530

    add

---
+++ src/components/Buto.jsx
@@ -0,0 +1,3 @@
+export default function Buto({ children }) {
+  return <button>{children}</button>;
+}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,12 @@
+import Buto from "./Buto";
+
 export default function Player() {
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
         <input type="text" />
-        <button>Set Name</button>
+        <Buto>Set Name</Buto>
       </p>
     </section>
   );
