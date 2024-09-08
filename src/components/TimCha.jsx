7e6eef3291305a17baa1afeea27c2076f5fd6fe1
kachraz <kachraz@tuta.io>
2024-09-08 19:09:14 +0530

    add

--- src/components/TimCha.jsx
+++ src/components/TimCha.jsx
@@ -1,16 +1,32 @@
 // Time challenge functin
 
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timSta, setTimSta] = useState(false);
+  const [timExp, setTimExp] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimExp(true);
+    }, targetTime * 1000);
+
+    setTimSta(true);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timExp && <p>FUCKED!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>StartPanty</button>
+        <button onClick={handleStart}>{timSta ? "Stop" : "Start"}Panty</button>
       </p>
-      <p className="">StartSniffing... / StopSniffing</p>
+      <p className={timSta ? "active" : undefined}>
+        {timSta ? "Sniffing..." : "SniffInactive"}
+      </p>
     </section>
   );
 }
